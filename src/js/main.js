0149287da8c8a6c73d7cf3993c7cb8781e74ad40
AlejandraSC <alejandrasalmeron6@gmail.com>
2020-10-10 14:37:05 +0200

    collapsablesJS

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//FORMULARIO
 const inputName = document.querySelector(".js-inputName");
 const inputPosition = document.querySelector(".js-inputPosition");
 const inputEmail = document.querySelector(".js-inputEmail");
@@ -13,7 +14,6 @@ const cardEmail = document.querySelector(".js-cardEmail");
 const cardPhone = document.querySelector(".js-cardPhone");
 const cardLinkedin = document.querySelector(".js-cardLinkedin");
 const cardGithub = document.querySelector(".js-cardGithub");
-
 
 function changeName() {
   cardName.innerHTML = inputName.value;
@@ -50,3 +50,20 @@ function changeGithub() {
   cardGithub.setAttribute("href", (cardGithub.href + inputGithub.value));
 }
 inputGithub.addEventListener("keyup", changeGithub);
+
+
+//COLAPSABLES
+const designContainer = document.querySelector(".js-designContainer");
+const fillContainer = document.querySelector(".js-fillContainer");
+const shareContainer = document.querySelector(".js-shareContainer");
+
+const designCollapsable = document.querySelector(".js-designCollapsable");
+const fillCollapsable = document.querySelector(".js-fillCollapsable");
+const shareCollapsable = document.querySelector(".js-shareCollapsable");
+
+
+function hideFill() {
+  fillContainer.classList.add("hidden");
+  console.log("hola");
+}
+fillCollapsable.addEventListener("click", hideFill);

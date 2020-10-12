d91b601b9f957f8e5fb87b8c382815d635061f56
MaribelLopezRomero <maribel.romero.miv@gmail.com>
2020-10-12 11:27:20 +0200

    Add js

--- src/js/main.js
+++ src/js/main.js
@@ -61,17 +61,24 @@ const designCollapsable = document.querySelector(".js-designCollapsable");
 const fillCollapsable = document.querySelector(".js-fillCollapsable");
 const shareCollapsable = document.querySelector(".js-shareCollapsable");
 
+const hiddenshare = document.querySelector ('.js-hiddenshare');
+
 function hideDesign() {
   designContainer.classList.toggle("hidden");
+  designCollapsable.classList.toggle("rotate");
 }
 designCollapsable.addEventListener("click", hideDesign);
 
 function hideFill() {
   fillContainer.classList.toggle("hidden");
+  fillCollapsable.classList.toggle("rotate");
 }
 fillCollapsable.addEventListener("click", hideFill);
 
 function hideShare() {
   shareContainer.classList.toggle("hidden");
+  hiddenshare.classList.toggle ('hiddenshare');
+  shareCollapsable.classList.toggle("rotate");
 }
 shareCollapsable.addEventListener("click", hideShare);
+

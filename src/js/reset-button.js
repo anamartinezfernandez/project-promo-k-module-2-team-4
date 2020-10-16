053fe25fee8a159ab74c8b7eea6597cef6bdd416
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-16 14:58:47 +0200

    Fix url in background image and border-left in card

--- src/js/reset-button.js
+++ src/js/reset-button.js
@@ -1,29 +1,26 @@
-const buttonReset = document.querySelector('.js-button--reset');
+const buttonReset = document.querySelector(".js-button--reset");
 const profileImageCard = document.querySelector(".js__profile-image");
-const url = "../assets/images/lisa_simpson.png";
+const url = "./assets/images/lisa_simpson.png";
 const smallImage = document.querySelector(".js__profile-preview");
 const urlSmall = " ";
 
-
-
 function reset() {
-  inputName.value = '';
+  inputName.value = "";
   cardName.innerHTML = "Lisa Simpson";
-  inputPosition.value = '';
+  inputPosition.value = "";
   cardPosition.innerHTML = "Saxophonist";
-  inputEmail.value = '';
+  inputEmail.value = "";
   cardEmail.href = "mailto:";
-  inputPhone.value = '';
+  inputPhone.value = "";
   cardPhone.href = "tel:";
-  inputLinkedin.value = '';
+  inputLinkedin.value = "";
   cardLinkedin.href = "https://";
-  inputGithub.value = '';
+  inputGithub.value = "";
   cardGithub.href = "https://";
 
   profileImageCard.style.backgroundImage = `url(${url})`;
 
   smallImage.style.backgroundImage = `url(${urlSmall})`;
-
 }
 
-buttonReset.addEventListener('click', reset);
+buttonReset.addEventListener("click", reset);
